e0076e960a1af5d7c968d110d1155e469d85138d
Felipe Celestino <felipe.rce@gmail.com>
2026-04-21 00:24:47 -0300

    Remove Compose JVM application module and add Kotlin Serialization plugin to build configuration

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -5,11 +5,12 @@ plugins {
     alias(libs.plugins.composeMultiplatform)
     alias(libs.plugins.composeCompiler)
     alias(libs.plugins.composeHotReload)
+    alias(libs.plugins.kotlinSerialization)
 }
 
 kotlin {
     jvm()
-    
+
     sourceSets {
         commonMain.dependencies {
             implementation(libs.compose.runtime)
@@ -18,8 +19,11 @@ kotlin {
             implementation(libs.compose.ui)
             implementation(libs.compose.components.resources)
             implementation(libs.compose.uiToolingPreview)
+            implementation(compose.materialIconsExtended)
             implementation(libs.androidx.lifecycle.viewmodelCompose)
             implementation(libs.androidx.lifecycle.runtimeCompose)
+            implementation(libs.androidx.navigation.compose)
+            implementation(libs.kotlinx.serialization.core)
         }
         commonTest.dependencies {
             implementation(libs.kotlin.test)
@@ -34,11 +38,11 @@ kotlin {
 
 compose.desktop {
     application {
-        mainClass = "io.github.feliperce.MainKt"
+        mainClass = "io.github.feliperce.mavenkeeper.MainKt"
 
         nativeDistributions {
             targetFormats(TargetFormat.Dmg, TargetFormat.Msi, TargetFormat.Deb)
-            packageName = "io.github.feliperce"
+            packageName = "MavenKeeper"
             packageVersion = "1.0.0"
         }
     }
--- composeApp/src/jvmMain/kotlin/io/github/feliperce/Platform.kt
+++
@@ -1,7 +0,0 @@
-package io.github.feliperce
-
-class JVMPlatform {
-    val name: String = "Java ${System.getProperty("java.version")}"
-}
-
-fun getPlatform() = JVMPlatform()
